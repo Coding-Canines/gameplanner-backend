63c9ea45ae24621075779628dae3d2f68cbdf36d
Aus <vargazalan58@gmail.com>
2026-08-29 17:02:46 +0200

    Removed static content route

--- src/main/kotlin/com/codingcanines/plugins/Routing.kt
+++ src/main/kotlin/com/codingcanines/plugins/Routing.kt
@@ -3,16 +3,15 @@ package com.codingcanines.plugins
 import com.codingcanines.repositories.users.UserRepository
 import com.codingcanines.routes.authRoutes
 import com.codingcanines.routes.userRoutes
-import io.ktor.http.ContentType
-import io.ktor.openapi.OpenApiInfo
+import io.ktor.http.*
+import io.ktor.openapi.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
-import io.ktor.server.http.content.*
 import io.ktor.server.plugins.di.*
-import io.ktor.server.plugins.swagger.swaggerUI
+import io.ktor.server.plugins.swagger.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.server.routing.openapi.OpenApiDocSource
+import io.ktor.server.routing.openapi.*
 
 fun Application.configureRouting() {
     val userRepository: UserRepository by dependencies
@@ -32,9 +31,6 @@ fun Application.configureRouting() {
                 }
             }
         }
-
-        // Static plugin. Try to access `/static/index.html`
-        staticResources("/static", "static")
 
         authRoutes(userRepository = userRepository)
 
