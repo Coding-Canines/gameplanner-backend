e17fdf6eb8b0900555e9121219bb715d4a8f762f
Aus <vargazalan58@gmail.com>
2026-08-29 18:04:26 +0200

    Set up Flyway migration on startup

--- src/main/kotlin/com/codingcanines/plugins/Dependencies.kt
+++ src/main/kotlin/com/codingcanines/plugins/Dependencies.kt
@@ -1,31 +1,28 @@
 package com.codingcanines.plugins
 
 import com.codingcanines.database.users.ExposedUserRepository
-import com.codingcanines.database.tables.Users
 import com.codingcanines.repositories.users.UserRepository
 import io.ktor.server.application.*
 import io.ktor.server.plugins.di.*
-import kotlinx.coroutines.launch
+import org.flywaydb.core.Flyway
 import org.jetbrains.exposed.v1.r2dbc.R2dbcDatabase
-import org.jetbrains.exposed.v1.r2dbc.SchemaUtils
-import org.jetbrains.exposed.v1.r2dbc.transactions.suspendTransaction
 
 fun Application.configureDependencies() {
     dependencies {
-        val database = R2dbcDatabase.connect(
-            url = System.getenv("DB_URL") ?: "r2dbc:postgresql://localhost:5432/ktor_db",
-            user = System.getenv("DB_USER") ?: "ktor_user",
-            password = System.getenv("DB_PASSWORD") ?: "ktor_password"
-        )
+        val r2dbcUrl = System.getenv("DB_URL") ?: "r2dbc:postgresql://localhost:5432/ktor_db"
+        val dbUser = System.getenv("DB_USER") ?: "ktor_user"
+        val dbPassword = System.getenv("DB_PASSWORD") ?: "ktor_password"
+
+        val jdbcUrl = r2dbcUrl.replace("r2dbc", "jdbc")
+        Flyway.configure()
+            .dataSource(jdbcUrl, dbUser, dbPassword)
+            .load()
+            .migrate()
+
+        val database = R2dbcDatabase.connect(url = r2dbcUrl, user = dbUser, password = dbPassword)
 
         provide<R2dbcDatabase> { database }
 
         provide<UserRepository> { ExposedUserRepository(database = resolve<R2dbcDatabase>()) }
-
-        launch {
-            suspendTransaction(database) {
-                SchemaUtils.create(Users)
-            }
-        }
     }
 }
